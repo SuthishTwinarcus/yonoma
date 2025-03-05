5d6e22d08f88fde4b12a07b1b0f47dc8ccc0d200
naveentwinarcus <naveen@twinarcus.com>
2025-03-05 18:57:01 +0530

    Update package with new features/fixes

--- tags.go
+++ tags.go
@@ -1,13 +1,6 @@
 package yonoma
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
+import "fmt"
 
 // TagParams for creating/updating tags
 type TagParams struct {
@@ -19,74 +12,12 @@ type Tag struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
-type Tags struct {
-	client TagsYonomaClient
-}
-type TagsYonomaClient struct {
-	apiKey  string
-	baseURL string
-	client  *http.Client
-}
-
-func (yc *TagsYonomaClient) Request(method, endpoint string, data interface{}) (map[string]interface{}, error) {
-	url := yc.baseURL + endpoint
-	var requestBody []byte
-	var err error
-
-	if data != nil {
-		requestBody, err = json.Marshal(data)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+yc.apiKey)
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := yc.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return nil, errors.New(string(body))
-	}
-
-	var responseData map[string]interface{}
-	err = json.Unmarshal(body, &responseData)
-	if err != nil {
-		return nil, err
-	}
-
-	return responseData, nil
-}
 
 // List all tags
-//
-//	func ListTags() ([]Tag, error) {
-//		//var tags []Tag
-//		var tags []Tag = []Tag{}
-//		err := request("GET", "tags/list", nil, &tags)
-//		return tags, err
-//	}
-func (t *Tags) ListTags() (map[string]any, error) {
-	fmt.Println("Created List")
-	//var tags []Tag
-	//var tags []Tag = []Tag{}
-	//err := request("GET", "tags/list", nil, &tags)
-	return t.client.Request("GET", "tags/list", nil)
+func ListTags() ([]Tag, error) {
+	var tags []Tag
+	err := request("GET", "/tags", nil, &tags)
+	return tags, err
 }
 
 // New creates a new tag
